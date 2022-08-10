25a2eb10920454e213d44ee3690681f1766d3f85
Abdul1042 <110098746+Abdul1042@users.noreply.github.com>
2022-08-10 13:22:25 +0530

    Add files via upload

---
+++ Validation.java
@@ -0,0 +1,54 @@
+package phase2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Validation
+ */
+//@WebServlet("/Validation")
+public class Validation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Validation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		PrintWriter print=response.getWriter();
+		String user=request.getParameter("user");
+		String pass=request.getParameter("pass");
+		String user1="Aj";
+		String pass1="A1042";
+		if(user.equals(user1) && pass.equals(pass1)) {
+			print.print("Sucessfully logged in");
+		}
+		else {
+			print.print("Failed in login");
+		}
+	}
+
+}
