00472bd97018ea1b9712721e8f268eb96431b135
Abdul1042 <110098746+Abdul1042@users.noreply.github.com>
2022-08-12 20:26:06 +0530

    Add files via upload

---
+++ Product_class.java
@@ -0,0 +1,21 @@
+package Practice2;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Product_class{
+	protected static Connection initializeDatabase()throws SQLException, ClassNotFoundException
+	{
+		String url = "jdbc:mysql://localhost:3306/db_world";
+	    String user = "root";
+	    String password = "root";
+	    Class.forName("com.mysql.jdbc.Driver");
+	    Connection con = DriverManager.getConnection(url, user, password);
+		return con;
+	}
+
+}
